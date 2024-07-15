5b6f2a4838dfc9cb383a6c9a8172cad19a952103
MuhammetKoca2810 <muhammetkoca2810@gmail.com>
2024-07-15 11:04:20 -0500

    home

--- @latest/src/components/Home.jsx
+++ @latest/src/components/Home.jsx
@@ -1,6 +1,31 @@
 import React from "react";
-
+import axios from "axios";
 
 const Home = () => {
-    
-}
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(
+        "https://precapstone-backend.onrender.com/api/user/users"
+      );
+    } catch (err) {
+      console.error("Error");
+    }
+  };
+
+  const handleDelete = (userId) => {
+    deleteUser(userId);
+  };
+
+  const handleUpdate = (userId) => {
+    updateUser(userId);
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleDelete(user.id)}>Delete</button>
+      <button onClick={() => handleUpdate(user.id)}>Update</button>
+    </div>
+  );
+};
+
+export default Home;
