e2aa6b87000da1ab9b0e026db2e0d779721cdb7d
ItsYaBalay <yassin1999@gmail.com>
2024-07-15 13:02:33 -0400

    completed user functions

---
+++ @latest/src/components/Update.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUpdateUserMutation } from "./userSlice";
+
+export default function UpdateUser() {
+  const [updateUser] = useUpdateUserMutation();
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const update = (e) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      let success = false;
+      success = await updateUser(form).unwrap();
+      if (success) {
+        navigate("/home");
+        console.log(success.token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div className="form-group">
+          <label>First Name</label>
+          <input
+            name="firstname"
+            value={form.firstname}
+            onChange={update}
+            type="text"
+            className="form-control"
+            placeholder="First Name"
+          />
+        </div>
+        <div className="form-group">
+          <label>Last Name</label>
+          <input
+            name="lastname"
+            value={form.lastname}
+            onChange={update}
+            type="text"
+            className="form-control"
+            placeholder="Last Name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Email address</label>
+          <input
+            name="email"
+            value={form.email}
+            onChange={update}
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={form.password}
+            onChange={update}
+            className="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
--- @latest/src/components/Users.jsx
+++ @latest/src/components/Users.jsx
@@ -1,24 +1,21 @@
 import { useGetUsersQuery, useDeleteUserMutation, useUpdateUserMutation } from "./userSlice";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Users() {
   const [users, getUser] = useState([]);
   const { data, isSuccess } = useGetUsersQuery();
-
+  const navigate = useNavigate();
   useEffect(() => {
     if (isSuccess) {
       getUser(JSON.parse(data));
     }
   }, [isSuccess]);
 
+  const [deleteUser] = useDeleteUserMutation();
+  
   const handleDelete = (userId) => {
-    //TODO delete user function
     deleteUser(userId);
-  };
-
-  const handleUpdate = (userId) => {
-    updateUser(userId);
-    //TODO update user function
   };
 
   return (
@@ -27,8 +24,8 @@ export default function Users() {
         users.map((user) => {
           return <p key={user.id}>{user.email}</p>;
         })}
-      <button onClick={() => handleDelete(user.id)}>Delete</button>
-      <button onClick={() => handleUpdate(user.id)}>Update</button>
+      <button onClick={() => handleDelete(users.id)}>Delete</button>
+      <button onClick={() => navigate("/update")}>Update</button>
     </div>
   );
 }
